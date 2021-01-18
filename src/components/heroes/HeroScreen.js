febf1db769cfb43d4341bdbcb488f541bf386eee
Andres Mestra <andresmestra11a@gmail.com>
2021-01-18 17:19:33 -0500

    obteniendo argumentos de la url

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,9 +1,30 @@
 import * as React from 'react'
+import { Redirect, useParams } from 'react-router-dom'
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+  
+  //optiene los argumentos de la url
+  const { heroeId } = useParams();
+
+  const hero = getHeroById(heroeId);
+  
+  //Redireccíona si el hero no existe
+  if( !hero ){
+    return <Redirect to="/" />
+  }
+
+  const { 
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters, 
+  } = hero;
+
   return (
     <div>
-      <h1>Hero Screen</h1>
+      <h1>{  }</h1>
     </div>
   )
 }
