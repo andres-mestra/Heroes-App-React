303e5525ce5d2b5c29b28e9b07e0bd784051a9b7
Andres Mestra <andresmestra11a@gmail.com>
2021-01-17 22:17:24 -0500

    Diferancias entre push y replace de react-router

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,25 @@
 import * as React from 'react'
 
-export const LoginScreen = () => {
+export const LoginScreen = ({ history }) => {
+
+  const handleLogin = () => {
+    /*Redirecciona  a la pagina indicada, si se presiona en el navegador 
+    regresar retorna a la pagina de origen ( LoginScrenn )*/ 
+    //history.push('/')
+    /*Redirecciona  a la pagina indicada, pero si se presiona en el navegador 
+    regresar NO retornara a  esta pagina ( LoginScrenn )*/
+    history.replace('/')
+  }
+
   return (
-    <div>
-      <h1>LoginScreen</h1>
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <button 
+        className="btn btn-primary"
+        onClick={ handleLogin }
+      >
+        login
+      </button>
     </div>
   )
 }
