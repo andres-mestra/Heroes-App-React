ec14a4204a41947d871a54764ee4d4e814cf1a07
Andres Mestra <andresmestra11a@gmail.com>
2021-01-19 12:31:07 -0500

    Configuración del context

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,10 +1,21 @@
 import * as React from 'react'
+import { authContext } from './auth/authContext'
+import { authReducer } from './auth/authReducer'
 import { AppRouter } from './routers/AppRouter'
 
+const init = () => {
+  return JSON.parse(localStorage.getItem('user')) || { logged:false }
+}
+
 export const HeroesApp = () => {
+  
+  const [ user , dispatch ] = React.useReducer(authReducer, {}, init)
+
   return (
     <main>
-      <AppRouter />
+      <authContext.Provider value={{ user, dispatch }} >
+        <AppRouter />
+      </authContext.Provider>
     </main>
   )
 }
