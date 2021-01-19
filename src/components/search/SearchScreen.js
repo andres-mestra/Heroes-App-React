4c0a9e609f9fbf12c228b54a406df152ba64181d
Andres Mestra <andresmestra11a@gmail.com>
2021-01-18 19:13:10 -0500

    Parse  url

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,14 +1,23 @@
 import *  as React from 'react'
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string'
 import { HeroListCard } from '../heroes/HeroListCard';
 import { useForm } from '../../hooks/useForm';
 import { heroes } from '../../data/heroes';
 
 
-export const SearchScreen = () => {
+
+export const SearchScreen = ({ history }) => {
+
+  const location = useLocation();
+
+  //parsea el q de la url, si no esta es q se le asigna por defecto el valor ''
+  const { q = ''} = queryString.parse(location.search);
 
   const [ heroesFilter, setHeroesFilter ] = React.useState([]);
-
-  const [ values, handleInputChange, reset ] = useForm()
+  const [ values, handleInputChange, reset ] = useForm({
+    search: q
+  })
   
   const { search } = values;
 
@@ -18,12 +27,15 @@ export const SearchScreen = () => {
     if( search < 3 ){
       return;
     }
-    setHeroesFilter( () => {
+    
+    //query selector en la url
+    history.push(`?q=${ search }`)
+    /* setHeroesFilter( () => {
       return heroes.filter( hero => { 
         return hero?.publisher.toLowerCase().includes(search) 
       }) 
     })
-    console.log(heroesFilter)
+    console.log(heroesFilter) */
   }
 
   return (
