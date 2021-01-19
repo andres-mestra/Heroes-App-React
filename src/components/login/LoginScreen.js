eb1a5a8fb8a2c2d71c065394f31f0afcdb211a3f
Andres Mestra <andresmestra11a@gmail.com>
2021-01-19 12:51:14 -0500

    simlación acción de login

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,14 +1,27 @@
 import * as React from 'react'
+import { authContext } from '../../auth/authContext'
+import { types } from '../../types/types';
 
 export const LoginScreen = ({ history }) => {
+
+  const { dispatch } = React.useContext( authContext );
 
   const handleLogin = () => {
     /*Redirecciona  a la pagina indicada, si se presiona en el navegador 
     regresar retorna a la pagina de origen ( LoginScrenn )*/ 
     //history.push('/')
+    
+    dispatch({
+      type: types.login,
+      payload: {
+        name: 'Andres'
+      }
+    })
+
     /*Redirecciona  a la pagina indicada, pero si se presiona en el navegador 
     regresar NO retornara a  esta pagina ( LoginScrenn )*/
     history.replace('/')
+
   }
 
   return (
